564209da7eba5ee9a3d09663e50e7bd43501bd7b
Wesley Hales <wesleyhales@gmail.com>
2011-11-18 13:50:39 -0500

    setup html5arch and branch slidfast lib into beginnings of book framework.

--- core/slidfast.js
+++ core/slidfast.js
@@ -25,118 +25,124 @@
  *
  *  version: 1.0.0.Alpha
  */
+(function(window,document,undefined){
 
-var slidfast = {
+    var slidfast = {
 
-    start: function() {
-        this.hideURLBar();
-        //set the default page
-        this.focusPage = document.getElementById(this.defaultPageID);
-        this.locationChange();
-    },
+        start: function() {
+            this.hideURLBar();
+            //set the default page
+            this.focusPage = document.getElementById(this.defaultPageID);
+            this.locationChange();
+        },
 
-    defaultPageID: null,
+        defaultPageID: null,
 
-    defaultPageHash: null,
+        defaultPageHash: null,
 
-    focusPage:  null,
+        focusPage:  null,
 
-    isReady: false,
+        isReady: false,
 
-    hideURLBar: function() {
-        //hide the url bar on mobile devices
-        setTimeout(scrollTo, 0, 0, 1)
-    },
+        hideURLBar: function() {
+            //hide the url bar on mobile devices
+            setTimeout(scrollTo, 0, 0, 1)
+        },
 
-    slideTo :   function (id) {
-        //check for double hash change call and no hash scenario on
-        //initial page load.
-        if (id == this.focusPage.id && (location.hash != "#" + this.defaultPageHash && location.hash != '')) {
-            return;
-        }
-        var focusPage = this.focusPage;
-        //1.)the page we are bringing into focus dictates how
-        // the current page will exit. So let's see what classes
-        // our incoming page is using. We know it will have stage[right|left|etc...]
-        var classes = document.getElementById(id).className.split(' ');
+        slideTo :   function (id) {
+            //check for double hash change call and no hash scenario on
+            //initial page load.
+            if (id == this.focusPage.id && (location.hash != "#" + this.defaultPageHash && location.hash != '')) {
+                return;
+            }
+            var focusPage = this.focusPage;
+            //1.)the page we are bringing into focus dictates how
+            // the current page will exit. So let's see what classes
+            // our incoming page is using. We know it will have stage[right|left|etc...]
+            var classes = document.getElementById(id).className.split(' ');
 
-        //2.)decide if the incoming page is assigned to right or left
-        // (-1 if no match)
-        var stageType = classes.indexOf('stage-left');
+            //2.)decide if the incoming page is assigned to right or left
+            // (-1 if no match)
+            var stageType = classes.indexOf('stage-left');
 
-        //3.) decide how this focused page should exit.
-        if (stageType > 0) {
-            focusPage.className = 'page transition stage-right';
-        } else {
-            focusPage.className = 'page transition stage-left';
-        }
-
-        //4. refresh/set the variable
-        focusPage = this.focusPage = document.getElementById(id);
-
-        //5. Bring in the new page and set the global.
-        focusPage.className = 'page transition stage-center';
-
-    },
-
-    init: function(defaultPageID, defaultPageHash) {
-        this.defaultPageID = defaultPageID;
-        this.defaultPageHash = defaultPageHash;
-
-        window.addEventListener('load', function(e) {
-            slidfast.isReady = true;
-            slidfast.start(slidfast.defaultPageID, slidfast.defaultPageHash);
-
-        }, false);
-        window.addEventListener('hashchange', function(e) {
-            slidfast.locationChange();
-        }, false);
-
-    },
-
-    locationChange: function() {
-        if (location.hash === "#" + this.defaultPageHash || location.hash == '') {
-            this.slideTo(this.defaultPageID);
-            //we're on the default page, so no need for back button
-            document.getElementById("back-button").className = 'hide-button';
-        } else {
-            var hashArray = location.hash.split(':');
-            var id;
-            var sample;
-            if (hashArray.length === 2) {
-                id = hashArray[0].replace('#', '');
-                sample = hashArray[1];
-                handleHashChange(id, sample);
-                //show the back button and attach functions
-                document.getElementById("back-button").className = 'basic-button left-header-button';
-                document.getElementById("back-button").onclick = function() {
-                    slidfast.slideTo(slidfast.defaultPageID);
-                    location.hash = slidfast.defaultPageHash;
-                };
+            //3.) decide how this focused page should exit.
+            if (stageType > 0) {
+                focusPage.className = 'page transition stage-right';
+            } else {
+                focusPage.className = 'page transition stage-left';
             }
 
+            //4. refresh/set the variable
+            focusPage = this.focusPage = document.getElementById(id);
+
+            //5. Bring in the new page and set the global.
+            focusPage.className = 'page transition stage-center';
+
+        },
+
+        init: function(defaultPageID, defaultPageHash) {
+            this.defaultPageID = defaultPageID;
+            this.defaultPageHash = defaultPageHash;
+
+            window.addEventListener('load', function(e) {
+                slidfast.isReady = true;
+                slidfast.start(slidfast.defaultPageID, slidfast.defaultPageHash);
+
+            }, false);
+            window.addEventListener('hashchange', function(e) {
+                slidfast.locationChange();
+            }, false);
+
+        },
+
+        locationChange: function() {
+            if (location.hash === "#" + this.defaultPageHash || location.hash == '') {
+                this.slideTo(this.defaultPageID);
+                //we're on the default page, so no need for back button
+                document.getElementById("back-button").className = 'hide-button';
+            } else {
+                var hashArray = location.hash.split(':');
+                var id;
+                var sample;
+                if (hashArray.length === 2) {
+                    id = hashArray[0].replace('#', '');
+                    sample = hashArray[1];
+                    handleHashChange(id, sample);
+                    //show the back button and attach functions
+                    document.getElementById("back-button").className = 'basic-button left-header-button';
+                    document.getElementById("back-button").onclick = function() {
+                        slidfast.slideTo(slidfast.defaultPageID);
+                        location.hash = slidfast.defaultPageHash;
+                    };
+                }
+
+            }
+        },
+
+        flip: function() {
+            //get a handle on the flippable region
+            var front = document.getElementById('front');
+            var back = document.getElementById('back');
+
+            //just a simple way to see what the state is
+            var classes = front.className.split(' ');
+            var flipped = classes.indexOf('flipped');
+
+            if (flipped >= 0) {
+                //already flipped, so return to original
+                front.className = 'normal';
+                back.className = 'flipped';
+            } else {
+                //do the flip
+                front.className = 'flipped';
+                back.className = 'normal';
+
+            }
         }
-    },
 
-    flip: function() {
-        //get a handle on the flippable region
-        var front = document.getElementById('front');
-        var back = document.getElementById('back');
+    };
 
-        //just a simple way to see what the state is
-        var classes = front.className.split(' ');
-        var flipped = classes.indexOf('flipped');
 
-        if (flipped >= 0) {
-            //already flipped, so return to original
-            front.className = 'normal';
-            back.className = 'flipped';
-        } else {
-            //do the flip
-            front.className = 'flipped';
-            back.className = 'normal';
+})(this,document);
 
-        }
-    }
 
-};
